39bacc21429f7d95b6807d36655af2b1057a9f83
叶同学 <jinzhanye9@gmail.com>
2020-05-08 22:51:09 +0800

    style: es lint 配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,26 @@
-import nodePolyfills from 'rollup-plugin-node-polyfills';
-import multi from '@rollup/plugin-multi-entry';
+import nodePolyfills from 'rollup-plugin-node-polyfills'
+import multi from '@rollup/plugin-multi-entry'
 import resolve from '@rollup/plugin-node-resolve'
-import babel from 'rollup-plugin-babel';
-import commonjs from '@rollup/plugin-commonjs';
-import { eslint } from "rollup-plugin-eslint";
+import babel from 'rollup-plugin-babel'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: 'src/**/*.js',
   output: {
     file: 'lib/bundle.js',
-    format: 'es'
+    format: 'es',
   },
   plugins: [
-    eslint(),
     resolve(),
     multi(),
     nodePolyfills(),
     commonjs({
-      include: "node_modules/**"
+      include: 'node_modules/**',
     }),
     babel({
       exclude: 'node_modules/**',
       runtimeHelpers: true,
     }),
   ],
-};
+}
 
